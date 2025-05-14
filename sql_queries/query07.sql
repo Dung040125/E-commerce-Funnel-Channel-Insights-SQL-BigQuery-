4a15991e862f5aef84ac6164c18cebcf9759271d
Thùy Dung <dungnguyenthi040125@gmail.com>
2025-05-14 13:58:29 +0700

    query07.sql

---
+++ sql_queries/query07.sql
@@ -0,0 +1,22 @@
+--Query 07: Other products purchased by customers who purchased product "YouTube Men's Vintage Henley" in July 2017. Output should show product name and the quantity was ordered.
+WITH buyer_list AS (
+  SELECT DISTINCT fullVisitorId
+  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
+       UNNEST(hits) AS hits,
+       UNNEST(hits.product) AS product
+  WHERE product.v2ProductName = "YouTube Men's Vintage Henley"
+    AND totals.transactions >= 1
+    AND product.productRevenue IS NOT NULL
+)
+
+SELECT
+  product.v2ProductName AS other_purchased_product,
+  SUM(product.productQuantity) AS quantity
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
+     UNNEST(hits) AS hits,
+     UNNEST(hits.product) AS product
+JOIN buyer_list USING(fullVisitorId)
+WHERE product.v2ProductName != "YouTube Men's Vintage Henley"
+  AND product.productRevenue IS NOT NULL
+GROUP BY other_purchased_product
+ORDER BY quantity DESC;
