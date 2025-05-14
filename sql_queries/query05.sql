ef7977d885e2184e3410ee68ddfd3e0ea8a9a2ec
Thùy Dung <dungnguyenthi040125@gmail.com>
2025-05-14 13:57:16 +0700

    query05.sql

---
+++ sql_queries/query05.sql
@@ -0,0 +1,10 @@
+--Query 05: Average number of transactions per user that made a purchase in July 2017
+SELECT
+  FORMAT_DATE('%Y%m', PARSE_DATE('%Y%m%d', date)) AS month,
+  ROUND(SUM(totals.transactions) / COUNT(DISTINCT fullVisitorId), 4) AS avg_transactions_per_user
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
+     UNNEST(hits) AS hits,
+     UNNEST(hits.product) AS product
+WHERE totals.transactions >= 1
+  AND product.productRevenue IS NOT NULL
+GROUP BY month;
